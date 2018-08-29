02e2e63756b2e1ba437d182e2e2fe205207ad7b8
devikareddybhimanadam <42571778+devikareddybhimanadam@users.noreply.github.com>
2018-08-29 16:40:38 +0530

    Rename alphabet.py to constant.py

---
+++ constant.py
@@ -0,0 +1,5 @@
+alph=raw_input()
+if alph in('a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'):
+	print("charecter is alphabet")
+else:
+	print("charecter is not a alphabet")
