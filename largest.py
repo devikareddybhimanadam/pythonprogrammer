a7befda5d9689b9d43402013fcf2f13d98cb105c
devikareddybhimanadam <42571778+devikareddybhimanadam@users.noreply.github.com>
2018-08-29 15:24:01 +0530

    Update largest.py

--- largest.py
+++ largest.py
@@ -1,10 +1,10 @@
-num1=int(input(""))
-num2=int(input(""))
-num3=int(input(""))
+num1=int(input())
+num2=int(input())
+num3=int(input())
 if(num1>=num2)and(num1>num3):
 	largest=num1
 elif(num2>=num1)and(num2>=num3):
 	largest=num2
 else:
 	largest=num3
-print("the largest number among",num1,",",num2,"and",num3,"is",largest) 
+print(largest) 
