ac6a2ed69ebc02e16e8041d8de8ec4505a3749cf
devikareddybhimanadam <42571778+devikareddybhimanadam@users.noreply.github.com>
2018-11-22 11:35:11 +0530

    Update 23.py

--- 23.py
+++ 23.py
@@ -1,9 +1,9 @@
 m= int(raw_input())
 def lowest(arr,n):
     m= arr[0]
-    for i in range(1, n):
-        if arr[i] < m:
-            m = arr[i]
+    for a in range(1, n):
+        if arr[a] < m:
+            m = arr[a]
     return m
 arr = [1,2,3,4,5]
 n = len(arr)
