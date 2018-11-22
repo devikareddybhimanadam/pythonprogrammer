9620200a8d525f02ee751842d115eb33e3e53009
devikareddybhimanadam <42571778+devikareddybhimanadam@users.noreply.github.com>
2018-11-22 11:09:02 +0530

    Update maximumelement.py

--- maximumelement.py
+++ maximumelement.py
@@ -1,9 +1,9 @@
 maximum = int(raw_input())
 def largest(arr,n):
     maximum = arr[0]
-    for i in range(1, n):
-        if arr[i] > maximum:
-            maximum = arr[i]
+    for a in range(1, n):
+        if arr[a] > maximum:
+            maximum = arr[a]
     return maximum
 arr = [1,2,3]
 n = len(arr)
