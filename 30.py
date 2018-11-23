a8b8f43a8112a6616012ff277e83259f8cb5380f
devikareddybhimanadam <42571778+devikareddybhimanadam@users.noreply.github.com>
2018-11-23 11:41:12 +0530

    Update 30.py

--- 30.py
+++ 30.py
@@ -1,21 +1,21 @@
 #include<stdio.h>
 int main(void)
 {
-	int s,p,r,q,a1,a2,t,n1,n2;
-	scanf("%d%d",&s,&p);
-	scanf("%d%d",&r,&q);
-	a1=s*60+p;
-	a2=r*60+q;
+	int i,j,k,l,b1,b2,t,n1,n2;
+	scanf("%d%d",&i,&j);
+	scanf("%d%d",&k,&l);
+	b1=i*60+j;
+	b2=k*60+l;
 	if(a1>a2)
 	{
-		t=a1-a2;
+		t=b1-b2;
 		n1=t/60;
 		n2=t%60;
 		printf("%d %d",n1,n2);
 	}
 	else
 	{
-		t=a2-a1;
+		t=b2-b1;
 		n1=t/60;
 		n2=t%60;
 		printf("%d %d",n1,n2);
